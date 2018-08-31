0d283df045bf96cbc3ef385559d91902a17d63a5
Macé Jérôme <angelshurikan@gmail.com>
2018-08-31 17:40:07 +0200

    update#

--- src/android/orion/Orion.java
+++ src/android/orion/Orion.java
@@ -12,7 +12,6 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import org.json.JSONArray;
